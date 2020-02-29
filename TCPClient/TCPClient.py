86e72b99e25da292dff360d9b7e1f67778920fb0
gokceozer <59254087+gokceozer@users.noreply.github.com>
2020-02-29 15:48:30 +0800

    Error Handling

--- TCPClient/TCPClient.py
+++ TCPClient/TCPClient.py
@@ -6,11 +6,13 @@ import os
 
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-serverName = "localhost"
+serverName = sys.argv[1]
 
-serverPort = 12000
+serverPort = sys.argv[2]
 
-clientSocket.connect( (serverName, serverPort) )
+clientSocket.connect( (serverName, int(serverPort)) )
+
+okay=True
 
 # Get input for sending
 username = input("Please input your user name:")
@@ -35,39 +37,63 @@ while(server_message.decode() != "1001 Authentication successful"):
 
     credentials ='/login ' + username + ' ' + password
 
-    clientSocket.send(credentials.encode())
+    try:
+        clientSocket.send(credentials.encode())
 
-    server_message = clientSocket.recv(1024)
+        server_message = clientSocket.recv(1024)
+
+    except BrokenPipeError as err:
+        print("Broken Pipe Error. Please restart server")
+        okay = False
+        break
+    except ConnectionResetError as err:
+        print("Server connection error. Please restart server")
+        okay = False
+        break
+    except KeyboardInterrupt as err:
+        print("Server has been interrupted via keyboard. Please restart server")
+        okay = False
+        break
 
 print(server_message.decode())
 
 def send_data():
-    #print("waiting for command:")
-    client_message = input()
-#    if client_message == "true" or client_message == "false":
-#        clientSocket.send(("guess " + client_message).encode())
-#    else:
-    clientSocket.send(client_message.encode())
+    while(True):
+    #    print("waiting for command:")
+
+    #    if client_message == "true" or client_message == "false":
+    #        clientSocket.send(("guess " + client_message).encode())
+    #    else:
+
+        client_message = input()
+        clientSocket.send(client_message.encode())
 
 
+x = threading.Thread(target=send_data)
+x.daemon = True
+x.start()
 
-while(server_message.decode() != "4001 Bye bye"):
+while(server_message.decode() != "4001 Bye bye" and okay == True):
 
-    x = threading.Thread(target=send_data)
-    x.daemon = True
-    x.start()
+    #client_message = input()
+    #clientSocket.send(client_message.encode())
 
     server_message = clientSocket.recv(1024)
     print(server_message.decode())
 
+    try:
+        if server_message.decode().split()[0] == "Server":
+            break
+    except IndexError as err:
+        print("Server has been shut down.")
+        break
+    except BrokenPipeError as err:
+        print("Broken Pipe Error. Please restart server")
+        break
+    #if server_message.decode() == "3011 Wait":
+    #    server_message = clientSocket.recv(1024)
+    #    print(server_message.decode())
 
-
-
-    #try:
-        #server_message = clientSocket.recv(1024)
-        #print(server_message.decode())
-    #except socket.timeout:
-    #    print("Didn't receive data! [Timeout 2s]")
 
 print("Client ends")
 clientSocket.close()
